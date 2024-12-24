a448b156b791117a28ea49874518d2587879f45b
Borto <mattiaborto71@gmail.com>
2024-12-24 11:28:04 +0100

    minor fixing, src and obj created

--- ABS/src/auto_diagnosi.s
+++ ABS/src/auto_diagnosi.s
@@ -7,7 +7,7 @@ wheel_labels: .asciz "1\0"
               .asciz "4\0"
 
 
-anomaly_msg .asciz "Anomalia rilevata, ||ABS NON IN FUNZIONE||ARRESTARE IL VEICOLO||"
+anomaly_msg: .asciz "Anomalia rilevata, ||ABS NON IN FUNZIONE||ARRESTARE IL VEICOLO||"
 
 .section .bss 
     speed_wheel_1: .space 4 // riservo 4 byte alla velocità della ruota 1 fino alla ruota 4
@@ -60,27 +60,27 @@ diagnose_wheels:
 
     BX LR // ritorno al chiamante
 
-check_wheel: // ho su R0 la velocita della ruota e su R1 il numero della ruota 
-    LDR  R2, [R0] // metto sul regsitro R2 la velocita della ruota 
-    LDR R3, =global_speed // metto su R3 l'indirizzo della velocita globale
-    LDR R3, [R4] // leggo il valore di velocita globale, su R3 avevo l'indirizzo e ora lo sto leggendo 
-    SUB R5, R2, R4 // calcolo la differenza tra singola ruota e velocita globale
-    CMP R5, #0 // ora controllo il segno della differenza
-    BGE positive_diff // salta se la differenza è positiva
-    RSBS R5, R5, #0 // rendo la differenza vel ruota - vel globale positiva 
-
+check_wheel: // ho su R0 la velocita della ruota e su R1 il numero della ruota
+    LDR  R2, [R0]       //carico la velcoità delle ruote su R2
+    LDR  R3, =global_speed
+    LDR  R3, [R3]       // Carico la velcoità globale su R3
+    SUB  R5, R2, R3     // Calcolo la differenza tra velocità di una ruota e velocità globale
+    CMP  R5, #0
+    BGE  positive_diff
+    RSBS R5, R5, #0     // Rendo la differenza positiva 
+   
 positive_diff:
-    LDR R6, =tollerance // carico la tolleranza nel registro di R6
-    LSR R6, [R6]
-    CMP R5, R6 // confronto la tolleranza con la differenza (vel ruota - vel globale) ovviamente postiva
-    BLS no_error  // ora guardo se non ci sono errori sulla tolleranza, e salto all'etichetta no_error
+    LDR  R6, =tolerance // carico la tolleranza
+    LSR  R6, R6, #1     // shifto R6 di 1 bit
+    CMP  R5, R6
+    BLS  no_error
 
-    // segnalo ERRORE NEI SENSORI
-    LDR R0, =error_message // carico sul registro R0 il messaggio di errore
-    BL print_string // stampo il messaggio di errore
-    LDR R0, =wheel_labels // carico l'indirizzo delle ruota
-    ADD R0, R0, R1, LSL #2 // calcolo l'indirizzo per l'offset in memoria per accedere ai sensori di una ruota specifica
-    BL print_string // ora stampo il numero della ruota
+    // Errore del segnale
+    LDR  R0, =error_message
+    BL   print_string
+    LDR  R0, =wheel_labels
+    ADD  R0, R0, R1, LSL #2
+    BL   print_string
 
 no_error:
     BL LR // ritorno al chiemante, tramite il registro LR
@@ -91,7 +91,7 @@ print_string:
 
  print_loop:
     LDRB R2, [R1], #1 // leggo di un byte e avanzo 
-    R2, #0 // controllo se il terminatore non è nullo 
+    CMP R2, #0 // controllo se il terminatore non è nullo 
     BEQ print_done // se è nullo ho finito e quindi salto a print_done
     MOV R7, #4  // chiamata a sistema della write 
     MOV R0, #1 // file descriptor di stdout
@@ -110,7 +110,7 @@ init_sensor: // etichetta che inizializza i sensori
     STR R0, [speed_wheel_4]
     STR R0, [global_speed]
     MOV R0, #1 // metto 1 nel registro  R1
-    STR r1. =engine_status // carico 1 nel registro engine status per simulare il motore acceso
+    STR r1, =engine_status // carico 1 nel registro engine status per simulare il motore acceso
     BX LR 
 
 end_program: // fine del programma 
